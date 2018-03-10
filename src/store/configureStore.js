2246fbc2a5fca698e6ab0261aae470e5af227bd9
Patti Rock <pmateosromero@gmail.com>
2018-03-10 09:17:19 +0100

    Create Redux store
    Create Redux store

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,26 +2,41 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import axios from 'axios';
 import axiosMiddleware from 'redux-axios-middleware';
-import rootReducer from '../reducers';
-import navigationMiddleware from '../middlewares/navigation';
+import rootReducer from '../modules';
+// import createHistory from 'history/createBrowserHistory';
+// import navigationMiddleware from '../middlewares/navigation';
+
+// export const history = createHistory();
 
 const client = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://api.themoviedb.org/3',
   responseType: 'json',
 });
 
-const middlewares = [
+const initialState = {};
+const enhancers = [];
+const middleware = [
   thunk,
   axiosMiddleware(client),
-  navigationMiddleware,
 ];
 
-export default function configureStore() {
-  return createStore(
-    rootReducer,
-    compose(
-      applyMiddleware(...middlewares),
-      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), // eslint-disable-line
-    ),
-  );
+if (process.env.NODE_ENV === 'development') {
+  const { devToolsExtension } = window;
+
+  if (typeof devToolsExtension === 'function') {
+    enhancers.push(devToolsExtension());
+  }
 }
+
+const composedEnhancers = compose(
+  applyMiddleware(...middleware),
+  ...enhancers,
+);
+
+const store = createStore(
+  rootReducer,
+  initialState,
+  composedEnhancers,
+);
+
+export default store;
