ba76ecc6eab15f047a8647e256b79ee7e3fc4ebf
Patti Rock <pmateosromero@gmail.com>
2018-03-10 09:19:17 +0100

    Remove a unnecessary module
    Remove Filters module

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
-import { reducers as filtersReducer } from '../modules/filters';
 import { reducers as movieReducer } from '../modules/movies';
 
 const rootReducer = combineReducers(Object.assign(
   {},
-  filtersReducer,
   movieReducer,
 ));
 
